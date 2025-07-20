346d734d712e2023eeb214175d8ac43aae239eae
MACB2403 <marioalbertocesen@gmail.com>
2025-07-20 13:05:19 -0500

    Se agrego la Clase de caracteres especiales

---
+++ Seccion01/src/package_01/Clase05CaracteresEspeciales.java
@@ -0,0 +1,20 @@
+package package_01;
+
+public class Clase05CaracteresEspeciales {
+    public static void main(String[] args) {
+        System.out.println("***CARACTERES ESPECIALES***");
+
+        System.out.println("SALTO DE LÍNEA");
+        System.out.println("Hola\nMundo");
+
+        System.out.println("TAB");
+        System.out.println("\tHola Mundo");
+
+        System.out.println("AGREGAR COMILLA DOBLE");
+        System.out.println("Hola \"Mundo\"");
+
+        System.out.println("AGREGAR DIAGONAL INVERTIDA ");
+        System.out.println("Hola \\Mundo\\");
+
+    }
+}
