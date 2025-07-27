845f24c2e406bae19160105f49746eab9844c796
MACB2403 <marioalbertocesen@gmail.com>
2025-07-27 11:44:39 -0500

    Se agrego el package general y un  ejemplo de encapsulamiento

---
+++ Seccion01/src/package_general/package_02/ClaseAritmetica.java
@@ -0,0 +1,33 @@
+package package_general.package_02;
+
+public class ClaseAritmetica {
+    // ATRIBUTOS
+    private int operador1; // encapsular por buenas prácticas
+    private int operador2; // encapsular por buenas prácticas
+    // CONSTRUCTORES
+    public ClaseAritmetica(int operador1,int operador2){
+        this.operador1=operador1;
+        this.operador2=operador2;
+    }
+    // MÉTODOS
+    public void sumar(){
+        int resultado= this.operador1+this.operador2;
+        System.out.println("\t\tResultado Sumar: "+resultado);
+    }
+    public void restar(){
+        int resultado= this.operador1-this.operador2;
+        System.out.println("\t\tResultado restar: "+resultado);
+    }
+    public int getOperador1(){
+        return this.operador1;
+    }
+    public void setOperador1(int operador1){
+        this.operador1=operador1;
+    }
+    public int getOperador2(){
+        return this.operador2;
+    }
+    public void setOperador2(int operador2){
+        this.operador2=operador2;
+    }
+}
---
+++ Seccion01/src/package_general/package_02/Main02.java
@@ -0,0 +1,22 @@
+package package_general.package_02;
+
+public class Main02 {
+    public static void main(String[] args) {
+        System.out.println("*** PAQUETES Y ENCAPSULAMIENTO ***");
+        System.out.println("\tValores iniciales");
+        ClaseAritmetica aritmetica1=new ClaseAritmetica(15,100);
+        System.out.println("\t\tDir. Memoria: "+aritmetica1);
+        System.out.println("\t\taritmetica1.getOperador1() = " + aritmetica1.getOperador1());
+        System.out.println("\t\taritmetica1.getOperador2() = " + aritmetica1.getOperador2());
+        aritmetica1.sumar();
+        aritmetica1.restar();
+        System.out.println("\tCambio de valores");
+        aritmetica1.setOperador1(3);
+        aritmetica1.setOperador2(4);
+        System.out.println("\t\tDir. Memoria: "+aritmetica1);
+        System.out.println("\t\taritmetica1.getOperador1() = " + aritmetica1.getOperador1());
+        System.out.println("\t\taritmetica1.getOperador2() = " + aritmetica1.getOperador2());
+        aritmetica1.sumar();
+        aritmetica1.restar();
+    }
+}
