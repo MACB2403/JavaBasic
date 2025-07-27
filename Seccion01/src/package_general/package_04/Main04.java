f0edd5539fc1b9d445862d31dcb071fc85e6a2b6
MACB2403 <marioalbertocesen@gmail.com>
2025-07-27 12:46:54 -0500

    Ejemplo de herencia, cambio de nombre de variable

--- Seccion01/src/package_general/package_04/Main04.java
+++ Seccion01/src/package_general/package_04/Main04.java
@@ -3,11 +3,11 @@ package package_general.package_04;
 public class Main04 {
     public static void main(String[] args) {
         System.out.println("*** HERENCIA ***");
-        ClaseAnimal animalgenerico=new ClaseAnimal();
+        ClaseAnimal animalGenerico=new ClaseAnimal();
         ClasePerro labrador=new ClasePerro();
-        System.out.println("\tAnimal Genérico = " + animalgenerico);
-        animalgenerico.comer();
-        animalgenerico.dormir();
+        System.out.println("\tAnimal Genérico = " + animalGenerico);
+        animalGenerico.comer();
+        animalGenerico.dormir();
         System.out.println("\tPerro = " + labrador);
         labrador.comer();
         labrador.dormir();
