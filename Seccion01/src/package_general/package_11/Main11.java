68ed410be5849cbb02d450bca7a48ae48a5d509f
MACB2403 <marioalbertocesen@gmail.com>
2025-07-29 21:08:14 -0500

    Ejemplo de for each

---
+++ Seccion01/src/package_general/package_11/Main11.java
@@ -0,0 +1,11 @@
+package package_general.package_11;
+
+public class Main11 {
+    public static void main(String[] args) {
+        System.out.println("*** FOR EACH ***");
+        int edades[] ={18,19,20,21,22};
+        for (int edad:edades){
+            System.out.print(edad+" ");
+        }
+    }
+}
