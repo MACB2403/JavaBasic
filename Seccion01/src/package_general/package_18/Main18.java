d27dada4b679942c7002e7a78857a925a8ac5d36
MACB2403 <marioalbertocesen@gmail.com>
2025-08-16 22:34:45 -0500

    Ejemplo de funciones lambda y métodos de referencia
    Adicionalmente se utiliza Arrays.alist para ingresar los datos desde el inicio

--- Seccion01/src/package_general/package_18/Main18.java
+++ Seccion01/src/package_general/package_18/Main18.java
@@ -1,19 +1,19 @@
 package package_general.package_18;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main18 {
     public static void main(String[] args) {
-        System.out.println("*** FUNCIONES LAMBDA ***");
+        System.out.println("*** FUNCIONES LAMBDA Y MÉTODOS DE REFERENCIA ***");
 
-        List<String> miListaA = new ArrayList<>();
-        miListaA.add("Enero");
-        miListaA.add("Febrero");
-        miListaA.add("Marzo");
+        List<String> miListaA = Arrays.asList("Enero","Febrero","Marzo");
 
         //Función lambda (función anónima, con código compacto)
         miListaA.forEach(elemento ->{System.out.println("Elemento: "+elemento);});
 
+        //Método de referencia
+        miListaA.forEach(System.out::println);
     }
 }
