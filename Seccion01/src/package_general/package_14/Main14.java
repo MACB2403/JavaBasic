516edcd191fef73c32cb3c093c6d5cbb9a0422c2
MACB2403 <marioalbertocesen@gmail.com>
2025-08-16 19:33:59 -0500

    Ejemplo de JavaBean
    El método toString no es necesario en un JavaBean

---
+++ Seccion01/src/package_general/package_14/Main14.java
@@ -0,0 +1,11 @@
+package package_general.package_14;
+
+public class Main14 {
+    public static void main(String[] args) {
+        System.out.println("*** JAVA BEANS ***");
+        Clase14JavaBeans persona = new Clase14JavaBeans();
+        persona.setNombre("Mario");
+        persona.setApellido("Cesén");
+        System.out.println("persona = " + persona);
+    }
+}
